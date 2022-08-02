e15b324b9140b7f1b59787645f89475733d1f806
zxx <xin.zhang@robotrak.cn>
2022-08-02 15:14:37 +0800

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,19 @@ const env = loadEnv('production', './');
 // vite.config.ts
 // Other dependencies...
 
+const desc = `
+// ==UserScript==
+// @name         spider-runtime
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
+`;
+
 export default defineConfig({
   root: './',
   build: {
@@ -23,18 +36,7 @@ export default defineConfig({
   },
   plugins: [
     banner({
-      content: `
-    // ==UserScript==
-    // @name         spider-runtime
-    // @namespace    http://tampermonkey.net/
-    // @version      0.1
-    // @description  try to take over the world!
-    // @author       You
-    // @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
-    // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
-    // @grant        none
-    // ==/UserScript==  
-    `,
+      content: desc,
     }),
   ],
 });
