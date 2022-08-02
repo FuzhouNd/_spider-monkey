a3d5fcd8332be23cd31678be0fc622e12bbdd272
zxx <xin.zhang@robotrak.cn>
2022-08-02 14:56:37 +0800

    dist

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         spider-runtime
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
 import { initSocket } from '@/browser/socket/index';
 initSocket()
 export default {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import {loadEnv} from 'vite';
+import { loadEnv } from 'vite';
+import banner from 'vite-plugin-banner';
 
-const env = loadEnv('production', './')
+const env = loadEnv('production', './');
 
+// vite.config.ts
+// Other dependencies...
 
 export default defineConfig({
   root: './',
@@ -12,10 +15,24 @@ export default defineConfig({
       entry: './src/browser/index.ts',
       name: 'spider_monkey_runtime',
       formats: ['iife'],
-      fileName:'runtime',
+      fileName: 'runtime',
     },
   },
   resolve: {
     alias: [{ find: '@/', replacement: path.resolve(__dirname, './src/') + '/' }],
   },
+  plugins: [
+    banner(`
+  // ==UserScript==
+  // @name         spider-runtime
+  // @namespace    http://tampermonkey.net/
+  // @version      0.1
+  // @description  try to take over the world!
+  // @author       You
+  // @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
+  // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+  // @grant        none
+  // ==/UserScript==  
+  `),
+  ],
 });
