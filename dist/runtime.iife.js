67e657afcfc7f4eed6cb4bf579535bb7b4d8b8db
zxx <xin.zhang@robotrak.cn>
2022-08-02 15:16:45 +0800

    dist

--- dist/runtime.iife.js
+++ dist/runtime.iife.js
@@ -5,7 +5,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
+// @match        *
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ const desc = `
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
+// @match        *
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
