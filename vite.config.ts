c73047da82354a3d54fc65e0ed732c8a0a320045
zxx <xin.zhang@robotrak.cn>
2022-08-02 15:04:45 +0800

    dost

--- vite.config.ts
+++ vite.config.ts
@@ -22,17 +22,19 @@ export default defineConfig({
     alias: [{ find: '@/', replacement: path.resolve(__dirname, './src/') + '/' }],
   },
   plugins: [
-    banner(`
- ==UserScript==
- @name         spider-runtime
- @namespace    http://tampermonkey.net/
- @version      0.1
- @description  try to take over the world!
- @author       You
- @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
- @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
- @grant        none
- ==/UserScript==  
-  `),
+    banner({
+      content: `
+    // ==UserScript==
+    // @name         spider-runtime
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  try to take over the world!
+    // @author       You
+    // @match        https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo/related?hl=zh-CN
+    // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+    // @grant        none
+    // ==/UserScript==  
+    `,
+    }),
   ],
 });
