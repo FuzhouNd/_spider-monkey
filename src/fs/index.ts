115c395e34c1ff04fdaacc76632e8baa66c55f44
zxx <xin.zhang@robotrak.cn>
2022-08-23 16:13:55 +0800

    feat: 小旺神

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -50,6 +50,7 @@ export function readFile(filePath: string) {
 export function readCsv<T = unknown>(filePath: string, options: csv.ParserOptionsArgs = { headers: true }): Promise<T[]> {
   return new Promise<T[]>((resolve, reject) => {
     if (!fs.pathExistsSync(filePath)) {
+      resolve([])
       return [];
     }
     const re: T[] = [];
