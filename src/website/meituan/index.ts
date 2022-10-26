6f846d3de7556b2271528dffcccbe16322519ab3
zxx <xin.zhang@robotrak.cn>
2022-10-26 14:37:26 +0800

    fix

--- src/website/meituan/index.ts
+++ src/website/meituan/index.ts
@@ -50,74 +50,77 @@ puppeteer.use(StealthPlugin());
 
   await cluster.task(async ({ page, data }) => {
     await page.goto(data.url2);
-    await delay(5000);
+    await delay(10000);
     let index = 1;
-    while (true) {
-      const imgUrl = await page.$eval('svgmtsi', (el) => {
-        const svgUrl = window.getComputedStyle(el).backgroundImage;
-        return svgUrl;
-      });
-      const url = imgUrl.slice(5, -2);
-      const res = await fetch(url, {
-        headers: {
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-          'accept-language': 'zh-CN,zh;q=0.9',
-        },
-        referrerPolicy: 'strict-origin-when-cross-origin',
-        body: null,
-        method: 'GET',
-      });
-      const svgText = await res.text();
-      const tList = await page.$$eval('.main-review', (elList) => {
-        return elList.map((el) => {
-          const wordsElem = el.querySelector('.review-words');
-          const name = el.querySelector('.name')?.textContent || '';
-          const imgUrlList = [...el.querySelectorAll('.review-pictures img')].map((img) => img.getAttribute('data-lazyload') || '');
-          const nameLink = el.querySelector('.name')?.href || '';
-          const time = el.querySelector('.time')?.textContent || '';
-          const shop = el.querySelector('.shop')?.textContent || '';
-          const textList = [...wordsElem.childNodes].map((child) => {
-            if (child.nodeName === 'SVGMTSI') {
-              const className = child.className;
-              const style = window.getComputedStyle(document.querySelector(`.${className}`));
-              return { x: style.backgroundPositionX.replace('px', ''), y: style.backgroundPositionY.replace('px', '') };
-            }
-            return { text: child.data };
-          });
-          return {
-            name: name.replace(/[\r\n\s]+/g, ''),
-            nameLink,
-            imgUrlList,
-            time: time.replace(/[\r\n\s]+/g, ''),
-            shop: shop.replace(/[\r\n\s]+/g, ''),
-            textList,
-          };
+
+    const imgUrl = await page.$eval('svgmtsi', (el) => {
+      const svgUrl = window.getComputedStyle(el).backgroundImage;
+      return svgUrl;
+    });
+    const url = imgUrl.slice(5, -2);
+    const res = await fetch(url, {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'zh-CN,zh;q=0.9',
+      },
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      body: null,
+      method: 'GET',
+    });
+    const svgText = await res.text();
+    const tList = await page.$$eval('.main-review', (elList) => {
+      return elList.map((el) => {
+        const wordsElem = el.querySelector('.review-words');
+        const name = el.querySelector('.name')?.textContent || '';
+        const imgUrlList = [...el.querySelectorAll('.review-pictures img')].map((img) => img.getAttribute('data-lazyload') || '');
+        const nameLink = el.querySelector('.name')?.href || '';
+        const time = el.querySelector('.time')?.textContent || '';
+        const shop = el.querySelector('.shop')?.textContent || '';
+        const textList = [...wordsElem.childNodes].map((child) => {
+          if (child.nodeName === 'SVGMTSI') {
+            const className = child.className;
+            const style = window.getComputedStyle(document.querySelector(`.${className}`));
+            return { x: style.backgroundPositionX.replace('px', ''), y: style.backgroundPositionY.replace('px', '') };
+          }
+          return { text: child.data };
         });
+        return {
+          name: name.replace(/[\r\n\s]+/g, ''),
+          nameLink,
+          imgUrlList,
+          time: time.replace(/[\r\n\s]+/g, ''),
+          shop: shop.replace(/[\r\n\s]+/g, ''),
+          textList,
+        };
       });
-      const dataList = tList.map((t) => {
-        const comment = t.textList
-          .map((node) => {
-            if (node.x) {
-              return getText(parseInt(node.x, 10), parseInt(node.y, 10), svgText);
-            }
-            return node.text;
-          })
-          .join('')
-          .replace(/[\r\n\s]+/g, '');
-        return R.omit(['textList'], { ...t, comment });
-      });
-      await writeCsv(path.resolve(__dirname, './ex.csv'), dataList);
-      const nextPage = await page.$('.NextPage');
-      if (nextPage) {
-        await nextPage.click();
-        index += 1;
-        data.url = data.url2 + ' page ' + index;
-        await delay(5000);
-      } else {
-        break;
-      }
-    }
+    });
+    const dataList = tList.map((t) => {
+      const comment = t.textList
+        .map((node) => {
+          if (node.x) {
+            return getText(parseInt(node.x, 10), parseInt(node.y, 10), svgText);
+          }
+          return node.text;
+        })
+        .join('')
+        .replace(/[\r\n\s]+/g, '');
+      return R.omit(['textList'], { ...t, comment });
+    });
+    await writeCsv(path.resolve(__dirname, './ex.csv'), dataList);
+
+    const tt = await page.$$eval('.PageLink', (dl) => {
+      const numbList = dl.map((d) => parseInt((d.textContent || '').replace(/[\s]*/g, ''), 10));
+      console.log(numbList);
+      const max = Math.max(...numbList);
+      return { max, url: (document.querySelector<HTMLLinkElement>('.PageLink')?.href || '').split('/').slice(0, -1).join('/') };
+    });
+    console.log(tt);
+    R.range(2, tt.max + 1).forEach((index) => {
+      const taskData = { url: tt.url + '/p' + index, url2: tt.url + '/p' + index };
+      // console.log(taskData);
+      // cluster.queue()
+    });
     // console.log(dataList);
   });
 
